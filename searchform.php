63be3b94e06e83b5c138e8a6bfd20c75bc32dd9c
George Paterson <paterson.george@googlemail.com>
2012-10-29 21:58:34 +0000

    Classnames for search form.

--- searchform.php
+++ searchform.php
@@ -4,8 +4,8 @@
  * @subpackage Aether
  */
 ?>
-<form action="/" method="get" accept-charset="utf-8" role="search">
+<form action="/" method="get" accept-charset="utf-8" id="search" class="search" role="search">
 	<label for="search-input">Search:</label>
 	<input type="text" name="s" id="search-input" value="<?php the_search_query(); ?>" />
-	<input type="submit" id="search-submit" role="button" />
+	<input type="submit" id="search-submit" class="button" role="button" />
 </form>
